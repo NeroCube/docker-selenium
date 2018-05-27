d1529bc1b2afb9b0030fb1b3a8be607f0312389c
Nero <nerocube.tw@gmail.com>
2018-05-27 23:14:08 +0800

    refactor tool.py

--- auto_browser/common/tool.py
+++ auto_browser/common/tool.py
@@ -1,30 +1,25 @@
-import datetime
+import datetime, requests, os
 
-def get_screenshot_file_path():
-    base_path ="./screenshot"
+def get_screenshot_file_path(filename="screenshot", filetype="png"):
+    base_path = "./screenshot"
     timestamp = datetime.datetime.today().strftime("%Y-%m-%d_%H:%M:%S.%f")
-    filename = "screenshot"
-    filetype = "png"
     return "{}/{}_{}.{}".format(base_path, timestamp, filename, filetype)
 
-def get_user_info():
-    email = "nerocube.tw@gmail.com"
-    password = "mypassword"
-    return {"email":email, "password":password}
+def mockup_user():
+    user = {}
+    user['email'] = "nerocube.tw@gmail.com"
+    user['password'] = "mypassword"
+    return user
 
-def mockup_answer():
-    answers = []
-    answer1 = 'answer'
-    answers.append(answer1)
-    return answers
-
-def mockup_headers():
-    headers = {'Content-Type':'application/json', 'Authorization': 'Token {}'.format("token")}
+def mockup_headers(content_type="application/json", token):
+    headers = {'Content-Type':content_type, 'Authorization': 'Token {}'.format(token)}
     return headers
 
-def mockup_test_info():
-    base_url = "base_url"
-    api_url = "api_url"
-    flag_login_success = "flag_login_success"
-    return {"base_url":base_url, "api_url":api_url, "flag_login_success":flag_login_success}
+def mockup_configs():
+    configs = {}
+    configs['base_url'] = "base_url"
+    configs['api_url'] = "api_url"
+    configs['auth_url'] = "auth_url"
+    configs['flag_login_success'] = "zEWidget-launcher"
+    return configs
 
